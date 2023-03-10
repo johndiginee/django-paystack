504b5c6589d4615cb91459b41dd192acd87a0809
John Diginee <Johndbizz@gmail.com>
2023-03-10 19:39:58 +0100

    Added views file

--- walletApp/views.py
+++ walletApp/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class Login(APIView):
+    permission_classes = ()
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key, "username": username})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Register(APIView):
+    authentication_classes = ()
+    permission_classes = ()
+
+    def post(self, request):
+
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
